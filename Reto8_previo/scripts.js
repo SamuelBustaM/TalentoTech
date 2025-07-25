694f275d1962e0f2ec69795a739f39a39e0a78da
SamuelBustaM <s.bustamante@udea.edu.co>
2025-07-25 12:17:47 -0500

    cambios

---
+++ Reto8_previo/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function() {
+    //Obtener la referencia de los elementos de la pagina
+    const inputBox =document.getElementById('inputBox');
+    const showBtn =document.getElementById('showBtn');
+    const outputBox =document.getElementById('outputBox');
+
+    //Añadir al boton un observador
+    showBtn.addEventListener('click', function(){
+        const message =inputBox.value;
+        console.log ('Dato ingresado',message)
+    })
+})
